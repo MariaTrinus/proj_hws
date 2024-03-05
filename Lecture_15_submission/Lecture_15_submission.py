7102e946e03704952742c60d90ce026dc4dee602
MariaTrinus <trinus_maria@ukr.net>
2024-03-05 17:49:05 +0200

    Add Lecture_15_submission/ 1.1

---
+++ Lecture_15_submission/Lecture_15_submission.py
@@ -0,0 +1,34 @@
+""" Лекція 15. DB """
+
+print(f"\n=======================| Task 1 |=======================")
+
+#   Create a data model for a AirBnb.com system.
+#       Your model should give ability to store information about
+#           the users
+#           the rooms
+#           the reservations
+#           and the reviews
+#       You should have two types of users:
+#           Hosts
+#           Guests
+#
+#   Host should be able
+#       to create rooms with different attributes (amount of residents, price, A/C, refrigerator, etc.)
+#   Guest should be able to make a
+#       Check availability of any roomsMake
+#       Reservation for a room.
+#
+#   For each table you should describe
+#       what is the primary key and
+#       what are the foreign keys (if any).
+#
+#   Result of the work might be description in a table.
+#   You can create tables in text file with description of each field.
+#   You also can you any graphic tool that you might use to create data model.
+#   I usually use DRAW.io for such thing, but might choose another tool.
+#
+#   (Optional): Add these possibilities for a guest
+#       Pay for reservation
+#       Review for the host.
+
+#   List of this tool: https://www.holistics.io/blog/top-5-free-database-diagram-design-tools/
